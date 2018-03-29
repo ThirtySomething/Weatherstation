eb9cf381b4f7c3df297ec6afa35f6e4e854d13fe
ThirtySomething <developer@derpaul.net>
2018-03-29 12:59:59 +0200

    - Setup runtime string in project files
    - Improve build for raspberry

--- build-rpi.bat
+++ build-rpi.bat
@@ -5,6 +5,7 @@ SET "STARTDIR=%~p0"
 REM Variable settings
 SET "DESTPATH=RPi-Build"
 SET "DESTARCH=linux-arm"
+REM SET "DESTARCH=linux-x64"
 SET "DESTFRAM=netcoreapp2.0"
 SET "DESTTYPE=Release"
 
@@ -17,16 +18,24 @@ REM Function DotNet Publish
 	IF EXIST "%1" (
 		ECHO.Publishing [%1]
 		CD %~dp1
-		dotnet publish -c %DESTTYPE% -f %DESTFRAM% --force -o "%STARTDIR%\\%DESTPATH%" -r %DESTARCH%
+		dotnet publish -c %DESTTYPE% -f %DESTFRAM% --self-contained --force -o "%STARTDIR%\\%DESTPATH%" -r %DESTARCH%
+	)
+EXIT /B
+
+REM Cleanup old crap
+:CLEANUP
+	IF EXIST "%STARTDIR%\\%DESTPATH%" (
+		RMDIR /s /q "%STARTDIR%\\%DESTPATH%"
 	)
 EXIT /B
 
 REM For all projects in solution
 :START
+CALL :CLEANUP
 FOR /f "delims=" %%f IN ('DIR /b /s /a-d *.csproj') DO (
 	REM Publish project
-	CALL :dnp "%%f"
+	CALL :DNP "%%f"
 )
 
 REM Switch back to startup directory
-cd %STARTDIR%
+CD %STARTDIR%
