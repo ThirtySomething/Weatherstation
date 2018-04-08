ff6d217486421cde16dabf71ca9a897d786eb187
ThirtySomething <developer@derpaul.net>
2018-04-08 10:40:20 +0200

    - Improved build scripts

---
+++ buildall.bat
@@ -0,0 +1,19 @@
+@ECHO OFF
+REM Memorize master directory
+SET "STARTDIR=%~dp0"
+SET "GENERATOR=buildscript.bat"
+SET "ARCHTYPES=win-x64 linux-x64 linux-arm"
+
+GOTO :START
+
+REM Append directory to variable
+:APA
+	SET "TMPDIR=%~dpn1\\%~n2"
+	FOR /f %%F IN ("%TMPDIR%") DO SET "%3=%%~dpnF"
+EXIT /B
+
+:START
+CALL :APA %STARTDIR% %GENERATOR% RUNME
+FOR %%i IN (%ARCHTYPES%) DO (
+	CALL %RUNME% %%i
+)
