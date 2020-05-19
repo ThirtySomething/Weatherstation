9bd39ab0db620fcc8f13ae6833b89c143a7e10e7
ThirtySomething <developer@derpaul.net>
2020-05-19 12:55:45 +0200

    Updated SQL scripts.

--- Plugins/DataSink/Database/weatherdata.mariadb.sql
+++ Plugins/DataSink/Database/weatherdata.mariadb.sql
@@ -10,15 +10,24 @@ CREATE TABLE IF NOT EXISTS `MUnit` (
 	PRIMARY KEY (`ID`)
 ) COLLATE='utf8_general_ci' ENGINE=InnoDB;
 
+CREATE TABLE IF NOT EXISTS `MLocation` (
+	`ID` BIGINT(20) NOT NULL AUTO_INCREMENT,
+	`Name` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	PRIMARY KEY (`ID`)
+) COLLATE='utf8_general_ci' ENGINE=InnoDB;
+
 CREATE TABLE IF NOT EXISTS `MValue` (
 	`ID` BIGINT(20) NOT NULL AUTO_INCREMENT,
 	`TypeID` BIGINT(20) NOT NULL,
+	`LocationID` BIGINT(20) NOT NULL,
 	`Value` DOUBLE NOT NULL,
 	`UnitID` BIGINT(20) NOT NULL,
 	`RecordTime` TEXT NOT NULL DEFAULT current_timestamp(),
 	PRIMARY KEY (`ID`),
 	INDEX `FK_MValue_MType_TypeID` (`TypeID`),
+	INDEX `FK_MValue_MLocation_LocationID` (`LocationID`),
 	INDEX `FK_MValue_MUnit_UnitID` (`UnitID`),
 	CONSTRAINT `FK_MValue_MType_TypeID` FOREIGN KEY (`TypeID`) REFERENCES `mtype` (`ID`),
+	CONSTRAINT `FK_MValue_MLocation_LocationID` FOREIGN KEY (`LocationID`) REFERENCES `mtype` (`ID`),
 	CONSTRAINT `FK_MValue_MUnit_UnitID` FOREIGN KEY (`UnitID`) REFERENCES `munit` (`ID`)
 ) COLLATE='utf8_general_ci' ENGINE=InnoDB;
